0898fda22602500ff016b4c029813e42cf279b4a
root <root@DESKTOP-0DV93UC>
2025-05-11 23:59:57 +0800

    add asr_model

--- asr/models/asr_model.py
+++ asr/models/asr_model.py
@@ -1,1 +1,16 @@
+from typing import Dict
+
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from omegaconf import DictConfig
+from torch import Tensor
+from torch.optim import ASGD, SGD, Adadelta, Adagrad, Adam, Adamax, AdamW
+
+from asr.criterion import CRITERION_REGISTRY
+from asr.metrics import CharacterErrorRate, WordErrorRate
+from asr.optim import AdamP, Novograd, RAdam
+from asr.optim.scheduler import SCHEDULER_REGISTRY
+from asr.tokenizers.tokenizer import Tokenizer
+
 # pause here
